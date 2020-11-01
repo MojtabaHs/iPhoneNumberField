cc2ddc6d5db5d5611f76eb0e240cb6c2add566a6
Alex <alexkfine2@gmail.com>
2020-11-01 14:28:38 -0800

    Added syntax sugar for simpler Font initialization

---
+++ Sources/iPhoneNumberTextField/SyntaxSugar.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Kalil Fine on 11/1/20.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public extension UIFont {
+    /// Initializes a `UIFont` using the same system initializer syntax availible in `Font` 🔠🔡
+        /// - Parameters:
+        ///   - size: The font size as a `CGFloat`
+        ///   - weight: Font weight from the `UIFont.Weight` types
+        ///   - design: Font design from the `UIFontDescriptor.SystemDesign` options
+    convenience init? (size: CGFloat = 14, weight: UIFont.Weight = .regular, design: UIFontDescriptor.SystemDesign = .rounded) {
+        // Will be SF Compact or standard SF in case of failure.
+        // Just pass in proper inputs and this shit won't fail
+        if let descriptor = UIFont.systemFont(ofSize: size, weight: weight).fontDescriptor.withDesign(design) {
+            self.init(descriptor: descriptor, size: size)
+        } else {
+            self.init()
+        }
+    }
+}
