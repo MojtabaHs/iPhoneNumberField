fc810af3759dee8e983f5de61f16eaf1d9eb5d5d
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2022-08-08 13:17:03 +0430

    add: Demo app

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 8/8/22.
+//
+
+import SwiftUI
+import iPhoneNumberField
+
+struct ContentView: View {
+    @State var text = ""
+    @State var isEditing = false
+    
+    var body: some View {
+        ZStack {
+            Color
+                .yellow
+                .ignoresSafeArea()
+                .onTapGesture { isEditing = false }
+            
+            iPhoneNumberField(
+                "Title",
+                text: $text,
+                isEditing: $isEditing,
+                formatted: true) {
+                    $0.numberPlaceholderColor = .red
+                    $0.textColor = .label
+                    $0.numberPlaceholderColor = .label
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
